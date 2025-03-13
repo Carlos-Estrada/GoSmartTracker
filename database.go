a127f8a5ab35deaeb9c0949ed100518b88b9f05a
Carlos Enrique R. Estrada <22361939+Carlos-Estrada@users.noreply.github.com>
2025-03-13 14:36:25 +0300

    Pushed database.go

---
+++ database.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func initDB() *sql.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Could not open database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	fmt.Println("Successfully connected to the database.")
+	return db
+}
+
+func main() {
+	db := initDB()
+	defer db.Close()
+}
